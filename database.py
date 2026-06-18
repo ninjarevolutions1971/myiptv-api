93680ef59b2a63842e8caac205eb15b166fe841c
ninjarevolutions1971 <danyleleriz@gmail.com>
2026-06-18 18:51:43 +0200

    Passaggio a PostgreSQL

--- database.py
+++ database.py
@@ -1,12 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./myiptv.db"
+DATABASE_URL = "postgresql://myiptv_user:VeKf23sPA6s1ccrgLg7I3FJijkG7FDzf@dpg-d8q1u8r6sc1c73b0g6cg-a.ohio-postgres.render.com/myiptv"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
