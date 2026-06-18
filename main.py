fe953a3ceaab45c3feaf2124f63eadfb34f11720
ninjarevolutions1971 <danyleleriz@gmail.com>
2026-06-18 22:32:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -61,3 +61,41 @@ def get_users():
     db.close()
 
     return result
+@app.post("/playlists")
+def create_playlist(playlist: PlaylistCreate):
+    db = SessionLocal()
+
+    new_playlist = Playlist(
+        name=playlist.name,
+        url=playlist.url
+    )
+
+    db.add(new_playlist)
+    db.commit()
+    db.refresh(new_playlist)
+
+    db.close()
+
+    return {
+        "id": new_playlist.id,
+        "name": new_playlist.name,
+        "url": new_playlist.url
+    }
+    @app.get("/playlists")
+def get_playlists():
+    db = SessionLocal()
+
+    playlists = db.query(Playlist).all()
+
+    result = [
+        {
+            "id": p.id,
+            "name": p.name,
+            "url": p.url
+        }
+        for p in playlists
+    ]
+
+    db.close()
+
+    return result
