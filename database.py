0eb28946ee22a84b4104be73a935c569f1fc5acd
ninjarevolutions1971 <danyleleriz@gmail.com>
2026-06-19 02:45:57 +0200

    Update DATABASE_URL for database connection

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://myiptv_user:VeKf23sPA6s1ccrgLg7I3FJijkG7FDzf@dpg-d8q1u8r6sc1c73b0g6cg-a.ohio-postgres.render.com/myiptv"
+DATABASE_URL = "postgresql://myiptv_user:KtKM4xLJjmzZPakocr8EieyuzHKZ4ddB@dpg-d8q8npojs32c738td8j0-a/myiptv_8v0t"
 
 engine = create_engine(
     DATABASE_URL,
